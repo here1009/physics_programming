6f7590f9a449d4de42f7d6f991808b4fd155872e
here1009 <gao_yantao@yeah.net>
2019-04-11 23:24:58 +0800

    update

--- mesh.cpp
+++ mesh.cpp
@@ -9,16 +9,12 @@ grid1D::grid1D()
 	dr=0.001;
 	mesh_size=100000;
 	mesh.resize(mesh_size);
+	fr.resize(mesh_size);
+	//
+	fp=[](double x)->double{return cos(x);};
 	for(int i=0;i<mesh_size;++i)
 	{
 		mesh[i]=i*dr+r0;
-	}
-	//
-	fp=[](double x)->double{return cos(x);};
-        //	
-	fr.resize(mesh_size);
-	for(int i=0;i<mesh_size;++i)
-	{
 		fr[i]=fp(i*dr+r0);
 	}
 }
